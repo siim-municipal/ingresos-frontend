001d33454cec677ebbcbfffbde5fce4d6b5b9823
Hugo Cobos <hcobos@azurian.com>
2026-01-14 11:18:15 -0600

    chore: initial workspace setup with nx, angular 21, zoneless and tailwind

---
+++ apps/cajas-app/src/app/app.config.ts
@@ -0,0 +1,23 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withViewTransitions,
+} from '@angular/router';
+import { appRoutes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(
+      appRoutes,
+      withComponentInputBinding(),
+      withViewTransitions(),
+    ),
+  ],
+};
